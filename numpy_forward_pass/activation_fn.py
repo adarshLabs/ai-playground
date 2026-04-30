1ac4a6c60a929d9e98eb0fd9b605080983d2e515
Adarsh Tiwari <tiwariadarshformal@gmail.com>
2026-05-01 04:44:50 +0530

    Remove numpy forward pass implementation and activation functions

--- numpy_forward_pass/activation_fn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def relu(input):
-    return np.maximum(0, input)
-
-def sigmoid(input):
-    return 1/(1 + np.exp(-input))
-              
-def tanh(input):
-    return (np.exp(input) - np.exp(-input))/(np.exp(input) + np.exp(-input))
